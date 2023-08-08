48cfb0352a545d605cdb42c00e19c44d08aeae47
HMTEUP2023 <yogiawan506@gmail.com>
2023-08-08 22:39:00 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ========================== typing animation ============================
+var typed = new Typed('.typed', {
+    strings:['', '','Our Website Is Coming Soon'],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
